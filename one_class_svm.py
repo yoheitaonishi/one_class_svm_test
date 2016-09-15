5b395669905a66e10b0459fb4cb881253c4f3a2e
yoheitaonishi <onishiyoheita@gmail.com>
2016-09-15 01:02:18 -0400

    added space

--- one_class_svm.py
+++ one_class_svm.py
@@ -5,7 +5,9 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
 from sklearn import svm
+
 xx, yy = np.meshgrid(np.linspace(-5, 5, 500), np.linspace(-5, 5, 500))
+
 # Generate train data
 X = 0.3 * np.random.randn(100, 2)
 X_train = np.r_[X + 2, X - 2]
@@ -14,6 +16,7 @@ X = 0.3 * np.random.randn(20, 2)
 X_test = np.r_[X + 2, X - 2]
 # Generate some abnormal novel observations
 X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
+
 # fit the model
 clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
 clf.fit(X_train)
@@ -23,6 +26,7 @@ y_pred_outliers = clf.predict(X_outliers)
 n_error_train = y_pred_train[y_pred_train == -1].size
 n_error_test = y_pred_test[y_pred_test == -1].size
 n_error_outliers = y_pred_outliers[y_pred_outliers == 1].size
+
 # plot the line, the points, and the nearest vectors to the plane
 Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
